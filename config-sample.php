4f9aee49d4d733313a4533b988f35e56c06b0b5c
tetsuwo <tetsuwo.oishi@gmail.com>
2014-07-09 01:57:06 +0900

    設定ファイル追加、監視対象外追加

---
+++ config-sample.php
@@ -0,0 +1,13 @@
+<?php return array(
+    'includes' => array('_aws'),
+    'services' => array(
+        'default_settings' => array(
+            'params' => array(
+                'key'     => 'YOUR_AWS_ACCESS_KEY_ID',
+                'secret'  => 'YOUR_AWS_SECRET_ACCESS_KEY',
+                // OR: 'profile' => 'my_profile',
+                'region'  => 'ap-northeast-1'
+            )
+        )
+    )
+);
---
+++ get.php
@@ -0,0 +1,19 @@
+<?php
+
+// Include the SDK using the Composer autoloader
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+use Aws\Common\Aws;
+
+// Create a service locator using a configuration file
+$aws = Aws::factory(sprintf('%s/config.php', dirname(__FILE__)));
+
+// Get client instances from the service locator by name
+$client = $aws->get('s3');
+
+$iterator = $client->getIterator('ListObjects', array('Bucket' => 'creema-demo'));
+
+foreach ($iterator as $object) {
+    echo $object['Key'] . "\n";
+}
